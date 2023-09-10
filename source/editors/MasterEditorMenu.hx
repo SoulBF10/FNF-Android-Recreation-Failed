1267fcf6ecdfbc35c9cd10c8b7735c100a9be2e9
glauberb0802 <111463416+glauberb0802@users.noreply.github.com>
2023-09-10 20:09:13 -0300

    Add files via upload

--- source/editors/MasterEditorMenu.hx
+++ source/editors/MasterEditorMenu.hx
@@ -11,7 +11,10 @@ import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.math.FlxMath;
 import flixel.text.FlxText;
 import flixel.util.FlxColor;
-import flixel.sound.FlxSound;
+import flixel.system.FlxSound;
+#if MODS_ALLOWED
+import sys.FileSystem;
+#end
 
 using StringTools;
 
@@ -25,17 +28,16 @@ class MasterEditorMenu extends MusicBeatState
 		'Character Editor',
 		'Chart Editor'
 	];
-
 	private var grpTexts:FlxTypedGroup<Alphabet>;
+	private var directories:Array<String> = [null];
+
 	private var curSelected = 0;
+	private var curDirectory = 0;
+	private var directoryTxt:FlxText;
 
 	override function create()
 	{
-		Paths.clearStoredMemory();
-		Paths.clearUnusedMemory();
-
 		FlxG.camera.bgColor = FlxColor.BLACK;
-
 		#if desktop
 		// Updating Discord Rich Presence
 		DiscordClient.changePresence("Editors Main Menu", null);
@@ -57,11 +59,29 @@ class MasterEditorMenu extends MusicBeatState
 			grpTexts.add(leText);
 			leText.snapToPosition();
 		}
+		
+		#if MODS_ALLOWED
+		var textBG:FlxSprite = new FlxSprite(0, FlxG.height - 42).makeGraphic(FlxG.width, 42, 0xFF000000);
+		textBG.alpha = 0.6;
+		add(textBG);
 
+		directoryTxt = new FlxText(textBG.x, textBG.y + 4, FlxG.width, '', 32);
+		directoryTxt.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
+		directoryTxt.scrollFactor.set();
+		add(directoryTxt);
+		
+		for (folder in Paths.getModDirectories())
+		{
+			directories.push(folder);
+		}
+
+		var found:Int = directories.indexOf(Paths.currentModDirectory);
+		if(found > -1) curDirectory = found;
+		changeDirectory();
+		#end
 		changeSelection();
 
 		FlxG.mouse.visible = false;
-
 		super.create();
 	}
 
@@ -71,11 +91,20 @@ class MasterEditorMenu extends MusicBeatState
 		{
 			changeSelection(-1);
 		}
-
 		if (controls.UI_DOWN_P)
 		{
 			changeSelection(1);
 		}
+		#if MODS_ALLOWED
+		if(controls.UI_LEFT_P)
+		{
+			changeDirectory(-1);
+		}
+		if(controls.UI_RIGHT_P)
+		{
+			changeDirectory(1);
+		}
+		#end
 
 		if (controls.BACK)
 		{
@@ -84,8 +113,7 @@ class MasterEditorMenu extends MusicBeatState
 
 		if (controls.ACCEPT)
 		{
-			switch (options[curSelected])
-			{
+			switch(options[curSelected]) {
 				case 'Character Editor':
 					LoadingState.loadAndSwitchState(new CharacterEditorState(Character.DEFAULT_CHARACTER, false));
 				case 'Week Editor':
@@ -96,16 +124,15 @@ class MasterEditorMenu extends MusicBeatState
 					LoadingState.loadAndSwitchState(new DialogueCharacterEditorState(), false);
 				case 'Dialogue Editor':
 					LoadingState.loadAndSwitchState(new DialogueEditorState(), false);
-				case 'Chart Editor': // felt it would be cool maybe
+				case 'Chart Editor'://felt it would be cool maybe
 					LoadingState.loadAndSwitchState(new ChartingState(), false);
 			}
-
 			FlxG.sound.music.volume = 0;
 			#if PRELOAD_ALL
 			FreeplayState.destroyFreeplayVocals();
 			#end
 		}
-
+		
 		var bullShit:Int = 0;
 		for (item in grpTexts.members)
 		{
@@ -121,7 +148,6 @@ class MasterEditorMenu extends MusicBeatState
 				// item.setGraphicSize(Std.int(item.width));
 			}
 		}
-
 		super.update(elapsed);
 	}
 
@@ -136,4 +162,28 @@ class MasterEditorMenu extends MusicBeatState
 		if (curSelected >= options.length)
 			curSelected = 0;
 	}
+
+	#if MODS_ALLOWED
+	function changeDirectory(change:Int = 0)
+	{
+		FlxG.sound.play(Paths.sound('scrollMenu'), 0.4);
+
+		curDirectory += change;
+
+		if(curDirectory < 0)
+			curDirectory = directories.length - 1;
+		if(curDirectory >= directories.length)
+			curDirectory = 0;
+	
+		WeekData.setDirectoryFromWeek();
+		if(directories[curDirectory] == null || directories[curDirectory].length < 1)
+			directoryTxt.text = '< No Mod Directory Loaded >';
+		else
+		{
+			Paths.currentModDirectory = directories[curDirectory];
+			directoryTxt.text = '< Loaded Mod Directory: ' + Paths.currentModDirectory + ' >';
+		}
+		directoryTxt.text = directoryTxt.text.toUpperCase();
+	}
+	#end
 }
